0a8c8cf97081fb626f95e60f67abe2277d129294
PUNOLEE <punolee@outlook.com>
2019-06-02 23:47:36 -0500

    sec2

--- components/sectiontwo.js
+++ components/sectiontwo.js
@@ -65,12 +65,16 @@ class Section2 extends React.Component {
       ],
       selected: [0, 0, 0, 0, 0, 0, 0],
       open: false,
-      data: []
+      shuffle: true,
+      data: [],
+      selected_example: []
     };
+    this.example = {};
     this.handleClick = this.handleClick.bind(this);
     this.setWrapperRef = this.setWrapperRef.bind(this);
     this.handleClickOutside = this.handleClickOutside.bind(this);
     this.clicknews = this.clicknews.bind(this);
+    this.shuffle = this.shuffle.bind(this);
   }
   componentDidMount() {
     document.addEventListener("mousedown", this.handleClickOutside);
@@ -124,12 +128,25 @@ class Section2 extends React.Component {
         break;
     }
     this.setState({
-      data:d
-    })
+      data: d
+    });
+    let shuffled = d.sort(() => 0.5 - Math.random());
+    this.setState({
+      selected_example: shuffled.slice(0, 3)
+    });
   }
-  clicknews() {
+  clicknews(id) {
+    this.example = this.state.data.filter(d => d.uuid === id)[0];
     this.setState({ open: true });
   }
+  shuffle(){
+    let shuffled = this.state.data.sort(() => 0.5 - Math.random());
+    this.setState({
+      selected_example: shuffled.slice(0, 3)
+    });
+  }
+
+
   render() {
     const { hasError, idyll, updateProps, ...props } = this.props;
     return (
@@ -165,44 +182,33 @@ class Section2 extends React.Component {
             </div>
             <MyBarChart data={this.state.data} />
           </div>
+          {this.state.selected_example.length !== 0?
           <div className={"examples_title"}>
             <span>The Fake News</span>
-          </div>
+          </div>:null}
           <div className={"examples"}>
-            <div className={"example_content"} onClick={() => this.clicknews()}>
-              <p className={"example_title x-small"}>Title goes here</p>
-              <p className={"example_short x-small"}>
-                <span className={"highlight"}>
-                  Content goes here!!!want to say more lol what to write tho
-                </span>
-                <a>Read more</a>
-              </p>
-              <p className={"example_footer x-small"}>Time here/ source here</p>
-            </div>
-            <div className={"example_content"}>
-              <p className={"example_title x-small"}>Title goes here</p>
-              <p className={"example_short x-small"}>
-                <span className={"highlight"}>
-                  Content goes here!!!want to say more lol what to write tho
-                </span>
-                <a>Read more</a>
-              </p>
-              <p className={"example_footer x-small"}>Time here/ source here</p>
-            </div>
-            <div className={"example_content"}>
-              <p className={"example_title x-small"}>Title goes here</p>
-              <p className={"example_short x-small"}>
-                <span className={"highlight"}>
-                  Content goes here!!!want to say more lol what to write tho
-                </span>
-                <a>Read more</a>
-              </p>
-              <p className={"example_footer x-small"}>Time here/ source here</p>
-            </div>
+            {this.state.selected_example.length !== 0
+              ? this.state.selected_example.map(s => (
+                  <div
+                    className={"example_content"}
+                    onClick={() => this.clicknews(s.uuid)}
+                  >
+                    <p className={"example_title x-small"}>{s.title}</p>
+                    <p className={"example_short_e x-small"}>
+                      <p className={"highlight"}>{s.text}</p>
+                      <a>Read more</a>
+                    </p>
+                    <p className={"example_footer x-small"}>
+                      {s.published.substring(0, 9)}/ {s.site_url}
+                    </p>
+                  </div>
+                ))
+              : null}
           </div>
+          {this.state.selected_example.length !== 0?
           <div className={"examples_btn"}>
-            <button className={"btn"}>Shuffle</button>
-          </div>
+            <button className={"btn"} onClick={()=>this.shuffle()}>Shuffle</button>
+          </div>:null}
         </div>
         <div className={this.state.open ? "news is-visible" : "news"}>
           <div className={"news_content"} ref={this.setWrapperRef}>
@@ -212,27 +218,17 @@ class Section2 extends React.Component {
             >
               x
             </div>
-            <h4 className={"news_date"}>Time</h4>
-            <h1 className={"news_title"}>It should be long long title</h1>
-            <p className={"news_body"}>
-              My family recently moved to a new state and was blessed to find a
-              wonderful new church to attend near our home. There's only one
-              drawback. We have a young toddler, so we sit in the cry room
-              during mass. At our previous church, the cry room was a place for
-              us to practice church etiquette with our son so that we could
-              someday sit with the rest of the congregation without disturbing
-              the mass.
-              <br />
-              <br />
-              The culture at this church is different; the cry room seems more
-              like a playroom. Seeing all the other children running around
-              makes keeping my toddler sitting in the pew nearly impossible
-              (think major meltdowns). If we allow him to play with the other
-              children, we spend the mass feeling like we've failed as christian
-              parents. The result is that neither my husband nor I has felt
-              fully present at a mass in months. Do you have any suggestions for
-              how to reconcile this issue? -- missing mass
-            </p>
+            {this.example.length !== 0 ? (
+              <div>
+                <h4 className={"news_date"}>
+                  {this.example.published
+                    ? this.example.published.substring(0, 9)
+                    : this.example.published}
+                </h4>
+                <h1 className={"news_title"}>{this.example.title}</h1>
+                <p className={"news_body"}>{this.example.text}</p>
+              </div>
+            ) : null}
           </div>
         </div>
       </div>
